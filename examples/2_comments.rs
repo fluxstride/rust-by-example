0fd60e760dc0a0711a6bf893b07e933daf8bd111
Samuel Adekoya <samueladexx@gmail.com>
2025-01-14 12:54:26 +0100

    comments in rust

---
+++ examples/2_comments.rs
@@ -0,0 +1,15 @@
+fn main() {
+    /* Regular Comments */
+    /* ------ */
+    // Line comments which goes at the end of a line
+    /* Block comments which goes to its closing delimiter */
+
+    /* Doc Comments */
+    /* ------ */
+    /// This doc comment generates library docs for the item following it
+    fn _doc_commnent_1() {}
+
+    fn _doc_commnent_2() {
+        //! This doc comment generates library docs for the item it is enclosed in
+    }
+}
